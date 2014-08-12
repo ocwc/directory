e67aeca7d9da2136916a4c22d9e0616889e358b6
Jure Cuhalev <gandalfar@gmail.com>
2014-08-12 10:46:12 +0200

    implements menu

--- directory/directory/static/assets/js/script.js
+++ directory/directory/static/assets/js/script.js
@@ -1,3 +1,21 @@
 jQuery(document).ready(function($) {
+    /* navigation menu */
+    var ww = document.body.clientWidth;
+    if (ww < 640) {
+        $(".menu > li > a").click(function() {
+            $(this).parent("li").toggleClass('hover');
+            return false;
+        });
+    } else {
+        $('.menu li').hover(function() {
+            $(this).addClass('hover');
+        }, function() {
+            $(this).removeClass('hover');
+        });
+    }
+
+    $('.toggle-menu').click(function(){
+        $('.menu').toggle();
+    });
 	
 });
